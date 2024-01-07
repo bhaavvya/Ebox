8b6f5540b1d3c05fa68d8f332dbea336c88ea31a
Bhavya <110487270+bhaavvya@users.noreply.github.com>
2024-01-08 01:40:23 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 
 
 const Connection = () =>{
-    const DB_URL = 'mongodb+srv://bhavyadv:bhavyaverma@impbox.x8dsujl.mongodb.net/?retryWrites=true&w=majority'
+    const DB_URL = ''
     try {
         mongoose.connect(DB_URL,{useNewUrlParser : true})
         console.log("Database connected sucessfully")
